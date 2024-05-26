162c61d4ae5c3659e65c798a9976a48358cefdd0
vinicius viana <viniciuspro333@gmail.com>
2024-05-26 14:33:00 -0300

    adc bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="/css/style.css">
         <script src="/js/script.js"></script>
         <title>@yield('title')</title>
@@ -12,7 +13,7 @@
             <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="collapse navbar-collapse" id="navbar">
                     <a href="/" class="navbar-brand">
-                        <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+                        HDC EVENTS
                     </a>
                     <ul class="navbar-nav">
                         <li class="nav-item">
