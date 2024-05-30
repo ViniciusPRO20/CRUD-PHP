c768332b53487b215e3e2c4d39163b0d219d151e
vinicius viana <viniciuspro333@gmail.com>
2024-05-30 18:12:07 -0300

    adicionando projetos para o dashboard pessoal

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,10 @@ Route::get('/events/create', [EventController::class, 'create'])->middleware('au
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
 
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
 
 Route::get('/contact', function () {
 
     return view('contact');
 });
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+
