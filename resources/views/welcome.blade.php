936f3d43408af304970083d1d83dbb5dcc0a63c6
vinicius viana <viniciuspro333@gmail.com>
2024-05-30 15:58:02 -0300

    adicionando data no database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,13 +17,16 @@
             <div class="card col-md-3">
                 <img src="/img/events/{{$event->image}}" alt="{{$event->title}}">
                 <div class="card-body">
-                    <p class="card-date">10/09/24</p>
+                    <p class="card-date">{{date('d/m/y', strtotime($event->date))}}</p>
                     <h5 class="card-title">{{$event->title}}</h5>
                     <p class="card-participantes">x participantes</p>
                     <a href="/events/{{$event->id}}" class="btn btn-primary">saber mais</a>
                 </div>
             </div>
         @endforeach
+        @if (count($events) == 0)
+            <p>não há eventos disponiveis!</p>
+        @endif
     </div>
 </div>
 
