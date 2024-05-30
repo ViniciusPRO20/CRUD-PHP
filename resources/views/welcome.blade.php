e7ffcf054a9a3770793dbeb3279f133841ade6f2
vinicius viana <viniciuspro333@gmail.com>
2024-05-30 13:55:45 -0300

    atualização salvamento de dados no database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
     <div id="cards-container" class="row">
         @foreach ($events as $event)
             <div class="card col-md-3">
-                <img src="" alt="{{$event->title}}">
+                <img src="/img/js.png" alt="{{$event->title}}">
                 <div class="card-body">
                     <p class="card-date">10/09/24</p>
                     <h5 class="card-title">{{$event->title}}</h5>
